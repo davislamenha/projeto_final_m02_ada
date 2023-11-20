90e8245bcfa8884c8af7076d3a02537ef7359cba
Davis Sampaio <davislamenha@gmail.com>
2023-11-20 18:59:29 -0300

    adicionar, listar e deletar tarefas

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,42 @@
-export const createTask = () => {};
+import readlineSync from 'readline-sync';
+
+import tasks from '../data/db.js';
+import { generateId } from '../utils/idGenerator.js';
+
+export const createTask = () => {
+  const id = generateId();
+  const name = readlineSync.question(`Informe o nome da tarefa: `);
+
+  const newTask = {
+    id,
+    name,
+  };
+
+  tasks.push(newTask);
+};
 
 export const updateTask = (id) => {};
 
-export const deleteTask = (id) => {};
+export const deleteTask = (id) => {
+  const task = tasks.find(({ id: taskId }) => taskId === Number(id));
 
-export const getTask = () => {};
+  if (!task) {
+    throw new Error('Tarefa não encontrada');
+  }
+
+  const index = Number(id) - 1;
+  tasks.splice(index, 1);
+};
+
+export const getTask = () => {
+  for (var i = 0; i < tasks.length; i++) {
+    const { id, name } = tasks[i];
+    console.log(`
+        Tarefa ${id}
+        Id: ${id}
+        Nome: ${name}
+        `);
+  }
+};
 
 export const detailTask = (id) => {};
